22d32acf7372a1c00c7a2999e7f99b44f425833f
Robin Lenz <robin.lenz@5minds.de>
2024-08-12 10:35:05 +0200

    fix vue component export

--- ui/index.js
+++ ui/index.js
@@ -1,2 +1,10 @@
 /* Exports of all of the components in this node */
-export { default as DynamicForm } from './components/DynamicForm.vue'
+import DynamicForm from './components/DynamicForm.vue'
+
+export {
+    DynamicForm
+}
+
+export default {
+    'ui-dynamic-form': DynamicForm
+}
