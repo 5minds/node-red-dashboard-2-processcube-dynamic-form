f946ab3d336cb3020310ee7a9c92bb33fac8c1bf
Robin Lenz <robin.lenz@5minds.de>
2024-08-13 12:35:09 +0200

    fix module expor

--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,2 @@
 /* Exports of all of the components in this node */
-import UIDynamicForm from './components/UIDynamicForm.vue'
-
-export {
-    UIDynamicForm
-}
-
-export default {
-    'ui-dynamic-form': UIDynamicForm
-}
+export { default as UIDynamicForm } from './components/UIDynamicForm.vue'
