a80d27c0320bcfc8727c34234deaf2c3c39d2977
luisthieme <luis.thieme@5minds.de>
2024-08-19 09:00:45 +0200

    change back to new export

--- ui/index.js
+++ ui/index.js
@@ -1,12 +1,2 @@
 /* Exports of all of the components in this node */
-// NEW
-// export { default as UIDynamicForm } from './components/UIDynamicForm.vue'
-
-// OLD
-import UIDynamicForm from './components/UIDynamicForm.vue';
-
-export { UIDynamicForm };
-
-export default {
-    'ui-dynamic-form': UIDynamicForm,
-};
+export { default as UIDynamicForm } from './components/UIDynamicForm.vue';
