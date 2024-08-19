fae668e93c1520ddf22ff09537630addf4bb7f00
luisthieme <luis.thieme@5minds.de>
2024-08-19 08:52:41 +0200

    remove need for default validation condition

--- ui/index.js
+++ ui/index.js
@@ -1,2 +1,12 @@
 /* Exports of all of the components in this node */
-export { default as UIDynamicForm } from './components/UIDynamicForm.vue'
+// NEW
+// export { default as UIDynamicForm } from './components/UIDynamicForm.vue'
+
+// OLD
+import UIDynamicForm from './components/UIDynamicForm.vue';
+
+export { UIDynamicForm };
+
+export default {
+    'ui-dynamic-form': UIDynamicForm,
+};
