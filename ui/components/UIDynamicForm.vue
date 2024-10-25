bac2046321dd640f6ac4a66b500471dae5677491
luisthieme <luis.thieme@5minds.de>
2024-10-25 13:33:58 +0200

    pass on props and socket obj

--- ui/components/UIDynamicForm.vue
+++ ui/components/UIDynamicForm.vue
@@ -3,18 +3,31 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, onMounted, ref, inject } from 'vue';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import UIDynamicForm from './UIDynamicForm.tsx';
 
 export default defineComponent({
     name: 'UIDynamicForm',
-    setup() {
+    inject: ['$socket'],
+    props: {
+        /* do not remove entries from this - Dashboard's Layout Manager's will pass this data to your component */
+        id: { type: String, required: true },
+        props: { type: Object, default: () => ({}) },
+        state: {
+            type: Object,
+            default: () => ({ enabled: false, visible: false }),
+        },
+    },
+    setup(props) {
         const reactRoot = ref(null);
+
+        const socket = inject('$socket');
+        
         onMounted(() => {
             const root = createRoot(reactRoot.value);
-            root.render(React.createElement(UIDynamicForm));
+            root.render(React.createElement(UIDynamicForm, {...props, socket}));
         });
         return {
             reactRoot,
