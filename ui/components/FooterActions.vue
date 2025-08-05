9b326263898ba9e4c2d506dc840e0d512a519bbf
Jeremy-Hill-02 <jeremy.hill.geschftl@gmail.com>
2025-08-05 14:22:13 +0200

    add suspend and terminate action

--- ui/components/FooterActions.vue
+++ ui/components/FooterActions.vue
@@ -1,44 +1,126 @@
 <template>
-    <div class="ui-dynamic-form-footer-actions-container">
-        <div class="ui-dynamic-form-footer-actions-left">
-            <div v-for="(action, index) in actions" :key="index">
-                <v-btn v-if="action.alignment === 'left' || !action.alignment" :key="index"
-                    class="ui-dynamic-form-footer-action-button" :class="getActionButtonClass(action)"
-                    :disabled="formIsFinished" @click="actionCallback(action)">
-                    {{ action.label }}
-                </v-btn>
-            </div>
-        </div>
-        <div class="ui-dynamic-form-footer-actions-right">
-            <div v-for="(action, index) in actions" :key="index">
-                <v-btn v-if="action.alignment === 'right'" :key="index" class="ui-dynamic-form-footer-action-button"
-                    :class="getActionButtonClass(action)" :disabled="formIsFinished" @click="actionCallback(action)">
-                    {{ action.label }}
-                </v-btn>
-            </div>
-        </div>
+  <div class="ui-dynamic-form-footer-actions-container">
+    <div class="ui-dynamic-form-footer-actions-left">
+      <div v-for="(action, index) in leftSideActions" :key="`left-${index}`">
+        <v-btn
+          class="ui-dynamic-form-footer-action-button"
+          :class="getActionButtonClass(action)"
+          :disabled="formIsFinished"
+          @click="actionCallback(action)"
+        >
+          {{ action.label }}
+        </v-btn>
+      </div>
+      <div>
+        <v-btn
+          class="ui-dynamic-form-footer-action-button ui-dynamic-form-footer-action-secondary"
+          :disabled="formIsFinished"
+          @click="handleSuspend"
+        >
+          Suspend
+        </v-btn>
+      </div>
+      <div>
+        <v-btn
+          class="ui-dynamic-form-footer-action-terminate"
+          :disabled="formIsFinished"
+          @click="handleTerminate"
+          variant="text"
+          size="small"
+        >
+          Terminate
+        </v-btn>
+      </div>
     </div>
+    <div class="ui-dynamic-form-footer-actions-right">
+      <div v-for="(action, index) in rightSideActions" :key="`right-${index}`">
+        <v-btn
+          class="ui-dynamic-form-footer-action-button"
+          :class="getActionButtonClass(action)"
+          :disabled="formIsFinished"
+          @click="actionCallback(action)"
+        >
+          {{ action.label }}
+        </v-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'UIDynamicFormFooterAction',
-    props: {
-        actions: { type: Array, default() { return [] } },
-        actionCallback: { type: Function, default(action) { } },
-        formIsFinished: { type: Boolean, default() { return false } }
+  name: 'UIDynamicFormFooterAction',
+  props: {
+    actions: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
-    methods: {
-        getActionButtonClass(action) {
-            return action.primary === 'true' ? 'ui-dynamic-form-footer-action-primary' : 'ui-dynamic-form-footer-action-secondary'
-        }
-    }
-}
+    actionCallback: { type: Function, default(action) {} },
+    formIsFinished: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+  },
+  computed: {
+    leftSideActions() {
+      const leftActions = this.actions.filter((action) => action.alignment === 'left' || !action.alignment);
+      const sortPrimaryToLeft = (a, b) => {
+        const aPrimary = a.primary === 'true';
+        const bPrimary = b.primary === 'true';
+        if (aPrimary && !bPrimary) return -1;
+        if (!aPrimary && bPrimary) return 1;
+        return 0;
+      };
 
+      return leftActions.sort(sortPrimaryToLeft);
+    },
+    rightSideActions() {
+      const rightActions = this.actions.filter((action) => action.alignment === 'right');
+      const sortPrimaryToRight = (a, b) => {
+        const aPrimary = a.primary === 'true';
+        const bPrimary = b.primary === 'true';
+        if (!aPrimary && bPrimary) return -1;
+        if (aPrimary && !bPrimary) return 1;
+        return 0;
+      };
+
+      return rightActions.sort(sortPrimaryToRight);
+    },
+  },
+  methods: {
+    getActionButtonClass(action) {
+      return action.primary === 'true'
+        ? 'ui-dynamic-form-footer-action-primary'
+        : 'ui-dynamic-form-footer-action-secondary';
+    },
+    handleTerminate() {
+      const terminateAction = {
+        label: 'Terminate',
+        alignment: 'left',
+        primary: 'false',
+        isTerminate: true,
+      };
+
+      this.actionCallback(terminateAction);
+    },
+    handleSuspend() {
+      const suspendAction = {
+        label: 'Suspend',
+        alignment: 'left',
+        primary: 'false',
+        isSuspend: true,
+      };
+
+      this.actionCallback(suspendAction);
+    },
+  },
+};
 </script>
 
 <style>
-/* CSS is auto scoped, but using named classes is still recommended */
 @import '../stylesheets/ui-dynamic-form.css';
 </style>
